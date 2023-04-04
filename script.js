0e274f1a19a6c75d2df19c2bfdc14c83f5a9840d
imshubhamSaha <imshubhamsaha@gmail.com>
2023-04-05 03:15:54 +0530

    Added newfeature with localStorage

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
+"use strict";
 const form = document.querySelector("#addForm");
 const message = document.querySelector("#displayAlert");
-const expenses = document.querySelector("#expenses");
 const expenseList = document.querySelector("#expenses");
 const expenseAmt = document.querySelector("#expenseamt");
-const expenseDes = document.querySelector("#expensedesc");
-const expenseCat = document.querySelector("#expensetype");
+const expenseComment = document.querySelector("#expensedesc");
+const expenseType = document.querySelector("#expensetype");
+const expenseCategory = document.querySelector("#expenseCategory");
+const expenseDate = document.querySelector("#expensedate");
 const expensesContainer = document.querySelector("#expensesCard");
 const formContainer = document.querySelector("#mainCard");
-const expenseFilter = document.querySelector("#expenseSearch");
+const expenseComFilter = document.querySelector("#expenseSearch");
+const filterByType = document.querySelector("#filtertype");
+const filterByCategory = document.querySelector("#filterCategory");
+const filterCatform = document.querySelector("#filCategory");
+const filterTypeform = document.querySelector("#filType");
+const searchMsgDiv = document.querySelector("#containerAlert");
 
-//Helper Class to manage edit state
-class State {
-  static state = "";
-  static userID = "";
+/////////////////////////////////////////Functions////////////////////////////////////////////////
+///////////////////////////Utility functions//////////////////////////////
+//Helper Obj to manage edit state
+const stateObj = {
+  stateName: "",
+  _id: "",
+};
 
-  static stateUpdate(key) {
-    State.state = key;
-  }
+///Set State
+const setState = function (state, id = "") {
+  stateObj.stateName = state;
+  stateObj._id = id;
+};
 
-  static userIDUpdate(id) {
-    State.userID = id;
-  }
-}
-
-//Functions
-////Helper functions
 // Message
 const errormsg = function (msg, clremove, cladd) {
   message.innerHTML = `${msg}`;
@@ -36,226 +41,304 @@ const errormsg = function (msg, clremove, cladd) {
   }, 2000);
 };
 
-///Access Localstore
-const storeData = function (obj) {
-  const localData = JSON.parse(localStorage.getItem("expenses"));
-  localData.push(obj);
-  localStorage.setItem("expenses", JSON.stringify(localData));
+//expenseContainer Hide and unhide
+const displayContainer = function (task, element = expensesContainer) {
+  if (task === "remove") element.classList.remove("d-none");
+  else if (task === "add") element.classList.add("d-none");
+};
+
+//Div Search Message display
+const searchMessage = function (msg, clremove, cladd) {
+  searchMsgDiv.innerHTML = `${msg}`;
+  searchMsgDiv.classList.remove(clremove);
+  searchMsgDiv.classList.add(cladd);
+};
+
+// div search message remove
+const removeSearchMsg = function (add) {
+  searchMsgDiv.classList.add(add);
+};
+
+//Check for field errors
+const util = function (formData) {
+  if (formData.money <= 0) {
+    errormsg("Please enter amount greater than 0", "d-none", "alert-danger");
+    return true;
+  } else if (formData.expensecomment === "") {
+    errormsg("Please enter Comment field", "d-none", "alert-danger");
+    return true;
+  } else if (formData.expensedate === "") {
+    errormsg("Please select Date", "d-none", "alert-danger");
+    return true;
+  }
 };
 
 //ShowonUi
-const ShowOnUi = function (obj) {
+const ShowOnUi = function (userData) {
   const li = document.createElement("li");
-  li.id = obj.id;
-  // Add class
+
   li.className = "list-group-item";
-  li.appendChild(document.createTextNode("Rs." + obj.money));
-  li.appendChild(document.createTextNode(" " + ": "));
-  li.appendChild(document.createTextNode(obj.expDescription + " : "));
-  li.appendChild(document.createTextNode(obj.expType));
+  li.classList.add("mb-2");
+  li.classList.add("border-1");
+  li.classList.add("border-dark-subtle");
+  li.classList.add("rounded");
+  li._id = userData._id;
+  const [year, month, day] = userData.expensedate.split("-");
+  const dateStr = new Date(year, `${month - 1}`, day);
 
-  //delbutton element
-  const deleteBtn = document.createElement("button");
-  deleteBtn.className = "btn btn-outline-danger btn-sm float-end delete";
-  deleteBtn.appendChild(document.createTextNode("Delete"));
-  li.appendChild(deleteBtn);
-  //editbutton element
-  const editBtn = document.createElement("button");
-  editBtn.className = "btn btn-outline-primary btn-sm  float-end edit me-1";
-  editBtn.appendChild(document.createTextNode("Edit"));
-  li.appendChild(editBtn);
+  const html = `
+    <div><strong>Expense Date : </strong>${dateStr.toDateString()}</div>
+    <div><strong>Expense Amount : </strong>Rs.${userData.money}</div>
+    <div><strong>Expense Type : </strong>${userData.expensetype}</div>
+    <div><strong>Expense Category : </strong>${userData.expensecategory}</div>
+    <div><strong>Expense Description : </strong>${userData.expensecomment}</div>
+    <div id="btnGrp" class="d-grid gap-2 mt-2 d-md-flex justify-content-md-end">
+      <button class="btn btn-outline-primary edit btn-sm" type="button">Edit</button>
+      <button class="btn btn-outline-danger delete btn-sm" type="button">Delete</button>
+    </div>
+  `;
 
+  li.insertAdjacentHTML("afterbegin", html);
   // Append li to list
-  expenses.appendChild(li);
+  expenseList.appendChild(li);
 };
 
-//localStorage set
-const initialised = function () {
-  State.stateUpdate("POST");
-  if (!localStorage.getItem("expenses"))
-    localStorage.setItem("expenses", JSON.stringify([]));
-  else {
-    const localData = JSON.parse(localStorage.getItem("expenses"));
-    if (!localData.length) return;
+// default form data
+const defaultFormData = function (data) {
+  expenseAmt.value = data?.money || "";
+  expenseComment.value = data?.expensecomment || "";
+  expenseType.value = data?.expensetype || "Card";
+  expenseCategory.value = data?.expensecategory || "Grocery";
+  expenseDate.value = data?.expensedate || "";
+};
 
-    localData.forEach((item) => {
-      ShowOnUi(item);
+/// Initial for every loading
+const init = function () {
+  setState("POST");
+  const local = localStorage.getItem("userData");
+  if (!local || JSON.parse(local).length === 0) return;
+
+  JSON.parse(local).forEach((item) => ShowOnUi(item));
+  displayContainer("remove");
+  errormsg("Database  Data are displayed in UI", "d-none", "alert-primary");
+};
+
+//to update UI
+const updateUI = function (arr, set) {
+  if (!set) {
+    arr.forEach((item) => item.classList.remove("d-none"));
+  } else {
+    arr.forEach((item) => {
+      if (!set.has(item._id)) item.classList.add("d-none");
+      else item.classList.remove("d-none");
     });
-    expensesContainer.classList.remove("d-none");
-    errormsg("Your Data displayed in UI", "d-none", "alert-primary");
   }
+  displayContainer("remove", expenseList);
+  removeSearchMsg("d-none");
 };
 
-//Delete from localStorage
-const deleteFromLocal = function (id) {
-  const updatedData = JSON.parse(localStorage.getItem("expenses")).filter(
-    (item) => item.id != id
+//Set localStorage Data
+const setLocalStore = function ({ ...data }) {
+  data._id = Date.now();
+  if (!localStorage.getItem("userData")) {
+    localStorage.setItem("userData", JSON.stringify([data]));
+  } else {
+    const localdata = JSON.parse(localStorage.getItem("userData"));
+    localdata.push(data);
+    localStorage.setItem("userData", JSON.stringify(localdata));
+  }
+  ShowOnUi(data);
+  if (expensesContainer.childElementCount === 1) displayContainer("remove");
+  defaultFormData();
+  errormsg("Data Added in Database", "d-none", "alert-primary");
+};
+
+//Edit localStorage DAta
+const editLocalStorage = function ({ ...data }) {
+  data._id = stateObj._id;
+
+  const localData = JSON.parse(localStorage.getItem("userData")).filter(
+    (item) => item._id !== data._id
   );
-  localStorage.setItem("expenses", JSON.stringify(updatedData));
+  localData.push(data);
+
+  localStorage.setItem("userData", JSON.stringify(localData));
+  ShowOnUi(data);
+  defaultFormData();
+  setState("POST");
+  errormsg("Data updated in the Database", "d-none", "alert-primary");
 };
 
-//Utility
-const util = function (obj) {
-  if (obj.money <= 0) {
-    errormsg("Please enter amount greater than 0", "d-none", "alert-danger");
-    return true;
-  } else if (obj.expDescription === "") {
-    errormsg("Please enter description field", "d-none", "alert-danger");
-    return true;
-  }
+//Remove LocalStorage Data
+const removeFromLocal = function (li) {
+  const filterData = JSON.parse(localStorage.getItem("userData")).filter(
+    (data) => data._id !== li._id
+  );
+  localStorage.setItem("userData", JSON.stringify(filterData));
+  expenseList.removeChild(li);
+  if (expenseList.childElementCount === 0) displayContainer("add");
+  errormsg("Data Deleted From Database", "d-none", "alert-danger");
 };
 
-// Adding new Data
-const newData = function (obj) {
-  if (util(obj)) return;
-
-  ShowOnUi(obj);
-
-  if (expenses.childElementCount === 1) {
-    expensesContainer.classList.remove("d-none");
-  }
-
-  // store in local storage
-  storeData(obj);
-
-  expenseAmt.value = "";
-  expenseDes.value = "";
-  expenseCat.value = "fuel";
-
-  //Alert
-  errormsg("Your Expense added successfully", "d-none", "alert-success");
+//FilteredData
+const filterData = function (val, index, totalData) {
+  const data = totalData
+    .filter(
+      (item) => [...item.children][index].childNodes[1].textContent === val
+    )
+    .map((item) => item._id);
+  return new Set(data);
 };
 
-//updating edited data
-const updateEditedData = function (obj) {
-  if (util(obj)) return;
-
-  async function asyncCall() {
-    const editData = JSON.parse(localStorage.getItem("expenses"));
-    for (const item of editData) {
-      if (item.id == obj.id) {
-        item.money = obj.money;
-        item.expDescription = obj.expDescription;
-        item.expType = obj.expType;
-      }
-    }
-
-    localStorage.setItem("expenses", JSON.stringify(editData));
-
-    ShowOnUi(obj);
-
-    if (expenses.childElementCount === 1) {
-      expensesContainer.classList.remove("d-none");
-    }
-
-    expenseAmt.value = "";
-    expenseDes.value = "";
-    expenseCat.value = "fuel";
-
-    //Alert
-    errormsg("Your Response Updated Successfully", "d-none", "alert-success");
-    State.stateUpdate("POST");
-    State.userIDUpdate("");
-  }
-  asyncCall();
+//////////////////////////CallBack Functions for event callback function ///////////////////////////
+// Adding new data
+const addNewData = function (userData) {
+  if (util(userData)) return;
+  setLocalStore(userData);
 };
 
-///Callback Functions
-//Add Expense
-const onSubmit = function (e) {
-  e.preventDefault();
+// Removing Existing Data
+const removeData = function (li) {
+  if (confirm("Are you Sure?")) removeFromLocal(li);
+};
 
-  const userData = {
-    id: State.state === "POST" ? Date.now() : State.userID,
-    money: expenseAmt.value,
-    expDescription: expenseDes.value,
-    expType: expenseCat.value,
+//Editing existing Data
+const editData = function (li) {
+  setState("PUT", li._id);
+  expenseList.removeChild(li);
+  if (expenseList.childElementCount === 0) displayContainer("add");
+
+  const [date, amt, type, cat, des] = [...li.children];
+  const editData = {
+    money: amt.childNodes[1].textContent.replaceAll("Rs.", ""),
+    expensecomment: des.childNodes[1].textContent,
+    expensetype: type.childNodes[1].textContent,
+    expensecategory: cat.childNodes[1].textContent,
+    expensedate: new Date(`${date.childNodes[1].textContent}"14:48:34 "`)
+      .toISOString()
+      .slice(0, 10),
   };
 
-  if (State.state === "POST") newData(userData);
-  else if (State.state === "PUT") updateEditedData(userData);
+  defaultFormData(editData);
+
+  errormsg(
+    "Your Previous Data will persist in Database untill you again submits",
+    "d-none",
+    "alert-primary"
+  );
 };
 
-// Remove Expense
-const removeItem = function (e) {
-  if (e.target.classList.contains("delete")) {
-    if (confirm("Are You Sure?")) {
-      const li = e.target.parentElement;
-      expenseList.removeChild(li);
-      if (expenses.childElementCount === 0) {
-        expensesContainer.classList.add("d-none");
-      }
-      //Deleting data from local
-      deleteFromLocal(li.id);
-    }
-  }
+// Filtering data against comments
+const filterComment = function (text) {
+  const totalChild = [...expenseList.children];
+  const li = totalChild
+    .filter(
+      (item) =>
+        [...item.children][4].childNodes[1].textContent
+          .toLowerCase()
+          .trim()
+          .indexOf(text) !== -1
+    )
+    .map((item) => item._id);
+
+  const hashSet = new Set(li);
+
+  if (!hashSet.size) {
+    displayContainer("add", expenseList);
+    searchMessage(
+      `Data with letter "${text}" not exist`,
+      "d-none",
+      "alert-danger"
+    );
+  } else updateUI(totalChild, hashSet);
 };
 
-//Edit Expense
-const editItem = function (e) {
-  if (e.target.classList.contains("edit")) {
-    State.stateUpdate("PUT");
-
-    const li = e.target.parentElement;
-    const editData = {
-      id: li.id,
-      money: e.target.parentElement.childNodes[0].data.replace("Rs.", ""),
-      expDescription: e.target.parentElement.childNodes[2].data.replace(
-        ":",
-        ""
-      ),
-      expType: e.target.parentElement.childNodes[3].data,
-    };
-
-    expenseList.removeChild(li);
-
-    if (expenses.childElementCount === 0) {
-      expensesContainer.classList.add("d-none");
-    }
-
-    expenseAmt.value = editData.money;
-    expenseDes.value = editData.expDescription;
-    expenseCat.value = editData.expType;
-
-    //update state storage
-    State.userIDUpdate(editData.id);
-  }
-};
-
-// search Expense
-const searchExpense = function (e) {
-  const text = e.target.value.toLowerCase().trim();
-
-  if (!expenses.childElementCount) {
-    errormsg("You don't have any saved data", "d-none", "alert-warning");
+//Filter BY Type Data
+const filtertypeFun = function (type) {
+  const allData = [...expenseList.children];
+  if (type === "All") {
+    updateUI(allData);
     return;
   }
-
-  const items = [...expenseList.childNodes];
-
-  const data = items.filter(
-    (item) => item.childNodes[3].data.trim().toLowerCase().indexOf(text) !== -1
-  );
-
-  if (data.length === 0) {
-    expensesContainer.classList.add("d-none");
-    errormsg(
-      `You don't have any saved data with letter ${text}`,
-      "d-none",
-      "alert-warning"
-    );
-  } else expensesContainer.classList.remove("d-none");
-
-  items.forEach((item) => {
-    const itemVal = item.childNodes[3].data.trim();
-    if (itemVal.toLowerCase().indexOf(text) == -1) item.classList.add("d-none");
-    else item.classList.remove("d-none");
-  });
+  const data = filterData(type, 2, allData);
+  if (!data.size) {
+    displayContainer("add", expenseList);
+    searchMessage(`Data with ${type} type not exist`, "d-none", "alert-danger");
+  } else {
+    updateUI(allData, data);
+  }
 };
 
-//event Handlers
-form.addEventListener("submit", onSubmit);
-expenseList.addEventListener("click", removeItem);
-expenseList.addEventListener("click", editItem);
-expenseFilter.addEventListener("keyup", searchExpense);
-window.addEventListener("DOMContentLoaded", initialised);
+//Filter BY Category Data
+const filterCategoryFun = function (cat) {
+  const allData = [...expenseList.children];
+  if (cat === "NoChoice") {
+    updateUI(allData);
+    return;
+  }
+  const data = filterData(cat, 3, allData);
+  if (!data.size) {
+    displayContainer("add", expenseList);
+    searchMessage(
+      `Data with ${cat} Category not exist`,
+      "d-none",
+      "alert-danger"
+    );
+  } else {
+    updateUI(allData, data);
+  }
+};
+
+////////////////////////////////Event CallBack Function/////////////////////////
+// Data adding event Callback
+const formSubmit = function (e) {
+  e.preventDefault();
+  const userData = {
+    money: expenseAmt.value,
+    expensecomment: expenseComment.value,
+    expensecategory: expenseCategory.value,
+    expensedate: expenseDate.value,
+    expensetype: expenseType.value,
+  };
+  if (stateObj.stateName === "POST") addNewData(userData);
+  else if (stateObj.stateName === "PUT") editLocalStorage(userData);
+};
+
+// Data(comment) matches Search
+const searchComment = function (e) {
+  const val = e.target.value.toLowerCase().trim();
+  if (!expenseList.childElementCount) {
+    errormsg("You don't have any saved data", "d-none", "alert-danger");
+    return;
+  }
+  filterComment(val);
+};
+
+//Data(filterType) matches
+const filterType = function (e) {
+  filterByCategory.value = "NoChoice";
+  const typeVal = e.target.value;
+  filtertypeFun(typeVal);
+};
+
+//Data(filterCategory) matches
+const filterCategory = function (e) {
+  filterByType.value = "All";
+  const categoryVal = e.target.value;
+  filterCategoryFun(categoryVal);
+};
+
+//Data removing and editing event Callback
+const universalBtn = function (e) {
+  const li = e.target.parentElement.parentElement;
+  if (e.target.classList.contains("delete")) removeData(li);
+  else if (e.target.classList.contains("edit")) editData(li);
+};
+
+//eventListener
+form.addEventListener("submit", formSubmit);
+expenseList.addEventListener("click", universalBtn);
+window.addEventListener("DOMContentLoaded", init);
+expenseComFilter.addEventListener("keyup", searchComment);
+filterByType.addEventListener("change", filterType);
+filterByCategory.addEventListener("change", filterCategory);
